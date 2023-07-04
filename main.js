6a0de2384d16a9dadd6938924c7265c128c8aacf
q.qaribiyan <gharibyan.ghader@gmail.com>
2023-07-04 12:09:35 +0200

    responive chart design

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ window.onload = () => {
         var parentDiv = document.getElementById("parent-ctr");
         // let width = 500 - margin.left - margin.right;
         // let width = parentDiv.clientWidth - 100;
-        let height = 500 - margin.top - margin.bottom;
+        let height = 360 - margin.top - margin.bottom;
         const windowWidthEm =
           window.innerWidth /
           parseFloat(
@@ -32,7 +32,7 @@ window.onload = () => {
         d3.select("#chart-container").selectAll("*").remove();
 
         if (windowWidthEm >= 70) {
-          width = 70 * 10
+          width = 70 * 10;
           // State 1: Default state with full spacing between bars
           // Create the SVG container
           const svg = d3
@@ -91,7 +91,7 @@ window.onload = () => {
             .text((d) => d["2022 (Billion kWh)"]);
         } else if (windowWidthEm >= 50) {
           console.log(windowWidthEm);
-          width = 50 * 10
+          width = 50 * 10;
           console.log("state 2");
           // State 2: Reduce spacing between bars, reduce bar width, rotate bar labels ~25°
           // Create the SVG container
@@ -157,11 +157,141 @@ window.onload = () => {
             .attr("text-anchor", "middle")
             .text((d) => d["2022 (Billion kWh)"]);
         } else if (windowWidthEm >= 40) {
+          width = 40 * 10;
           console.log("state 3");
+          // State 2: Reduce spacing between bars, reduce bar width, rotate bar labels ~25°
+          // Create the SVG container
+          const svg = d3
+            .select("#chart-container")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+          // Set up the x-scale
+          const x = d3
+            .scaleBand()
+            .range([0, width])
+            .padding(0.05)
+            .domain(data.map((d) => d["Energy sources"]));
+
+          // Set up the y-scale
+          const y = d3
+            .scaleLinear()
+            .range([height, 0])
+            .domain([0, d3.max(data, (d) => +d["2022 (Billion kWh)"])]);
+
+          const xAxis = d3.axisBottom(x);
+          svg
+            .append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(xAxis)
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.5em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-25)")
+            .style("font-size", "10px")
+            .style("padding-bottom", "10px");
+
+          // Add the y-axis
+          svg.append("g").call(d3.axisLeft(y));
+          barWidth = 50;
+          // Add the bars
+          svg
+            .selectAll(".bar")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr(
+              "x",
+              (d) => x(d["Energy sources"]) + (x.bandwidth() - barWidth) / 2
+            )
+            .attr("y", (d) => y(+d["2022 (Billion kWh)"]))
+            .attr("width", barWidth)
+            .attr("fill", "steelblue")
+            .attr("height", (d) => height - y(+d["2022 (Billion kWh)"]));
+          svg
+            .selectAll(".bar-label")
+            .data(data)
+            .enter()
+            .append("text")
+            .attr("class", "bar-label")
+            .attr("x", (d) => x(d["Energy sources"]) + x.bandwidth() / 2)
+            .attr("y", (d) => y(+d["2022 (Billion kWh)"]) - 5)
+            .attr("text-anchor", "middle")
+            .text((d) => d["2022 (Billion kWh)"]);
         } else if (windowWidthEm >= 30) {
           console.log("state 4");
+          width = 30 * 10;
+          // State 2: Reduce spacing between bars, reduce bar width, rotate bar labels ~25°
+          // Create the SVG container
+          const svg = d3
+            .select("#chart-container")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+          // Set up the x-scale
+          const x = d3
+            .scaleBand()
+            .range([0, width])
+            .padding(0.02)
+            .domain(data.map((d) => d["Energy sources"]));
+
+          // Set up the y-scale
+          const y = d3
+            .scaleLinear()
+            .range([height, 0])
+            .domain([0, d3.max(data, (d) => +d["2022 (Billion kWh)"])]);
+
+          const xAxis = d3.axisBottom(x);
+          svg
+            .append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(xAxis)
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.5em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-90)")
+            .style("font-size", "8px");
+
+          // Add the y-axis
+          svg.append("g").call(d3.axisLeft(y));
+          barWidth = 40;
+          // Add the bars
+          svg
+            .selectAll(".bar")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr(
+              "x",
+              (d) => x(d["Energy sources"]) + (x.bandwidth() - barWidth) / 2
+            )
+            .attr("y", (d) => y(+d["2022 (Billion kWh)"]))
+            .attr("width", barWidth)
+            .attr("fill", "steelblue")
+            .attr("height", (d) => height - y(+d["2022 (Billion kWh)"]));
+          svg
+            .selectAll(".bar-label")
+            .data(data)
+            .enter()
+            .append("text")
+            .attr("class", "bar-label")
+            .attr("x", (d) => x(d["Energy sources"]) + x.bandwidth() / 2)
+            .attr("y", (d) => y(+d["2022 (Billion kWh)"]) - 5)
+            .attr("text-anchor", "middle")
+            .text((d) => d["2022 (Billion kWh)"]);
         } else if (windowWidthEm >= 20) {
           console.log("s5");
+          width = 30 * 9
           // State 5: Switch to horizontal bars, reduce spacing between bars, reduce bar width, rotate bar labels ~60°, reduce font size of bar labels
           d3.select("#chart-container").selectAll("*").remove();
           const svg = d3
@@ -184,12 +314,16 @@ window.onload = () => {
             .domain([0, d3.max(data, (d) => +d["2022 (Billion kWh)"])]);
 
           // Add the y-axis
-          svg.append("g").call(d3.axisLeft(y));
+          svg.append("g")
+          .style("font-size", "8px")
+    
+          .call(d3.axisLeft(y));
 
           // Add the x-axis
           svg
             .append("g")
             .attr("transform", `translate(0, ${height})`)
+            
             .call(d3.axisBottom(x));
 
           // Add the bars
@@ -216,7 +350,7 @@ window.onload = () => {
             .attr("x", (d) => x(+d["2022 (Billion kWh)"]) + 5)
             .attr("text-anchor", "start")
             .text((d) => d["2022 (Billion kWh)"])
-            .style("font-size", "10px");
+            .style("font-size", "8px");
         }
       }
       updateChart();
